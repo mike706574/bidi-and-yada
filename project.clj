c6750eb75906c3d39ce496a8fba8ad10bf060096
Mike Easter <michael.easter@qg.com>
2017-04-11 10:21:43 -0500

    Update yada.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject mike/bidi-and-yada "0.0.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.taoensso/timbre "4.8.0"]
-                 [com.stuartsierra/component "0.3.1"]
-                 [aleph "0.4.1"]
-                 [yada "1.2.0"]
+                 [com.stuartsierra/component "0.3.2"]
+                 [aleph "0.4.3"]
+                 [yada "1.2.1"]
                  [bidi "2.0.16"]]
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
